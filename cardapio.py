c12e87ff303319e5793adf5e0135ee5d3c1cac12
Luane Gabrielly <luanegabrielly2202@gmail.com>
2026-04-13 21:17:27 -0300

    Add files via upload

---
+++ cardapio.py
@@ -0,0 +1,13 @@
+# Função Cardápio
+def exibirCardapio():
+    print("*" * 50)
+    print("*** Cardápio")
+    print("*" * 50)
+    print("1 Hamburger Max - R$15.00")
+    print("2 Hamburger Simples - R$8.00")
+    print("3 Batata Simples - R$13.00")
+    print("4 Finalizar Pedido")
+    print("*" * 50)
+
+# Exibir Principal
+exibirCardapio()
